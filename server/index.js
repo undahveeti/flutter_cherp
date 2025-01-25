e904acc6ec694e1de6ffe114cf5a6131d9657194
undahveeti <undahvt@gmail.com>
2025-01-24 16:23:22 -0800

    mongoDB connection implementation

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(authRouter);
+
+const DB = "mongodb+srv://avtran:test123@cherp.fn8os.mongodb.net/?retryWrites=true&w=majority&appName=Cherp"
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected at port ${PORT}`)
